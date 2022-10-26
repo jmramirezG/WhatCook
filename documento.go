50e13bfe30a1cba827efaa4ec7ebbfd349b21754
agony <agony@disroot.org>
2022-10-26 15:50:59 +0200

    Elimina la lógica de negocio como se explica en #9
    Elimina las dependencias y el método implementado
    en `documento.go` por no ser necesario para este
    objetivo como se explica en #9.

--- documento.go
+++ documento.go
@@ -1,24 +1,6 @@
 package WhatCook
 
-import (
-	"fmt"
-	. "pdftron"
-)
-
-import  "pdftron/Samples/LicenseKey/GO"
-
 type Documento struct {
 	path      string
 	reemplazo string
-}
-
-func (d Documento) generaDocumento(pathGuardado []string) ([]string) {
-	// Create a TemplateDocument object from an input office file.
-	templateDoc := ConvertCreateOfficeTemplate(d.path, NewOfficeToPDFOptions());
-
-	// Fill the template with data from a JSON string, producing a PDF document.
-	pdfDoc := templateDoc.FillTemplateJson(d.reemplazo);
-
-	// Save the PDF to a file.
-	pdfDoc.Save(pathGuardado, uint(SDFDocE_linearized))
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module WhatCook
 
 go 1.19
-
-require github.com/PDFTron/pdftron-go v1.1.0 // indirect
